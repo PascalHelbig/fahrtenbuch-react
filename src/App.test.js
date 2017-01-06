25f0e8f02d98139c4cef17a09125709b64d7b9e6
Pascal Helbig <pascalhelbig@gmail.com>
2017-01-06 18:33:50 +0100

    :green_heart: fix ci test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  // Wrapp into MuiThemeProvider, because App needs the theme context
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>,
+    div);
 });
