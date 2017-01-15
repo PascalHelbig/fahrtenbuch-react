34d7ad444e89db505ff8d6105c06305e7856cd58
Pascal Helbig <pascalhelbig@gmail.com>
2017-01-15 23:43:03 +0100

    handle changes in login form

--- src/auth/Login/Login.js
+++ src/auth/Login/Login.js
@@ -1,24 +1,47 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
 import Formsy from 'formsy-react';
 import EmailTextField from '../../textField/EmailTextField';
 import PasswordTextField from '../../textField/PasswordTextField';
 import Header from '../../Header/Header';
 
-const Login = ({ onLogin }) => (
-  <div>
-    <Header>Login</Header>
-    <Formsy.Form>
-      <EmailTextField />
-      <PasswordTextField />
-    </Formsy.Form>
-    <RaisedButton
-      label="login"
-      onTouchTap={() => onLogin('test@test.de', '12345')}
-      primary
-    />
-  </div>
-);
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: {},
+      isValid: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(values) {
+    this.setState({ values });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header>Login</Header>
+        <Formsy.Form
+          onChange={this.handleChange}
+          onValid={() => this.setState({ isValid: true })}
+          onInvalid={() => this.setState({ isValid: false })}
+        >
+          <EmailTextField />
+          <PasswordTextField />
+        </Formsy.Form>
+        <RaisedButton
+          label="login"
+          onTouchTap={() => this.props.onLogin('test@test.de', '12345')}
+          disabled={!this.state.isValid}
+          primary
+        />
+      </div>
+    );
+  }
+}
 
 Login.propTypes = {
   onLogin: PropTypes.func.isRequired,
--- src/auth/Login/Login.test.js
+++ src/auth/Login/Login.test.js
@@ -15,3 +15,19 @@ it('runs login on button touchTap', () => {
   wrapper.find(RaisedButton).simulate('touchTap');
   expect(onLogin).toHaveBeenCalled();
 });
+
+it('enable button on valid form', () => {
+  const wrapper = shallow(<Login onLogin={() => {}} />);
+  wrapper.setState({ isValid: true });
+
+  const loginButton = wrapper.find(RaisedButton);
+  expect(loginButton.prop('disabled')).toBeFalsy();
+});
+
+it('disable button on invalid form', () => {
+  const wrapper = shallow(<Login onLogin={() => {}} />);
+  wrapper.setState({ isValid: false });
+
+  const loginButton = wrapper.find(RaisedButton);
+  expect(loginButton.prop('disabled')).toBeTruthy();
+});
