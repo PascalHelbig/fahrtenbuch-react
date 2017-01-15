8eba36f8b808010cd5256100c4a774acac60b902
Pascal Helbig <pascalhelbig@gmail.com>
2017-01-16 00:00:40 +0100

    added tests to Login

--- src/auth/Login/Login.test.js
+++ src/auth/Login/Login.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
+import Formsy from 'formsy-react';
 import Login from './Login';
 
 it('renders correctly', () => {
@@ -30,4 +31,31 @@ it('disable button on invalid form', () => {
 
   const loginButton = wrapper.find(RaisedButton);
   expect(loginButton.prop('disabled')).toBeTruthy();
+});
+
+it('saves changes in state', () => {
+  const wrapper = shallow(<Login onLogin={() => {}} />);
+  wrapper.instance().handleChange({ email: 'test@test.de' });
+
+  expect(wrapper.state('values')).toEqual({ email: 'test@test.de' });
+});
+
+it('set isValid state to true, when form calls onValid()', () => {
+  const wrapper = shallow(<Login onLogin={() => {}} />);
+  const form = wrapper.find(Formsy.Form);
+
+  wrapper.setState({ isValid: false });
+  form.simulate('valid');
+
+  expect(wrapper.state('isValid')).toBeTruthy();
+});
+
+it('set isValid state to false, when form calls onInvalid()', () => {
+  const wrapper = shallow(<Login onLogin={() => {}} />);
+  const form = wrapper.find(Formsy.Form);
+
+  wrapper.setState({ isValid: true });
+  form.simulate('invalid');
+
+  expect(wrapper.state('isValid')).toBeFalsy();
 });
